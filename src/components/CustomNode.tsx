f195dcdde00717b5fd4c947f4413d907f63f7db5
Eugen Bondarev <eug.bondarev@gmail.com>
2026-09-12 14:43:31 +0200

    Highlighting selected node

--- src/components/CustomNode.tsx
+++ src/components/CustomNode.tsx
@@ -1,4 +1,5 @@
 import { Handle, Position, type Node, type NodeProps } from '@xyflow/react'
+import { cn } from '../lib/class'
 
 export type CustomNodeData = {
   label: string
@@ -8,9 +9,14 @@ export const CUSTOM_NODE_TYPE = 'CUSTOM_NODE'
 
 export type CustomNodeDefinition = Node<CustomNodeData, typeof CUSTOM_NODE_TYPE>
 
-export function CustomNode({ data }: NodeProps<CustomNodeDefinition>) {
+export function CustomNode({ data, selected }: NodeProps<CustomNodeDefinition>) {
   return (
-    <div className="rounded-lg border border-zinc-300 bg-white px-4 py-2 shadow w-50 h-50">
+    <div
+      className={cn(
+        'rounded-lg border border-zinc-300 bg-white px-4 py-2 shadow w-50 h-50',
+        selected && 'border-violet-500 ring-2 ring-violet-300',
+      )}
+    >
       <Handle type="target" position={Position.Left} />
       <span className="text-sm font-medium text-zinc-700">{data.label}</span>
       <Handle type="source" position={Position.Right} />
